da2774fa31e0771b76a21a0bcf552df002f81b90
Pavana Priya C <39463949+Pavana-Priya-C@users.noreply.github.com>
2024-08-29 19:29:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ import os
 from huggingface_hub import login
 
 huggingface_token = os.getenv("HUGGINGFACE_TOKEN")
+st.write(huggingface_token)
 login(huggingface_token)
 
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", use_auth_token=True)
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", use_auth_token=True)
+# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", use_auth_token=True)
+# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", use_auth_token=True)
 
 input_text='In evaluating the quality of machine-generated summaries, ROUGE & BERTScore are commonly used metrics, each with unique benefits. ROUGE (Recall-Oriented Understudy for Gisting Evaluation) emphasizes the syntactic overlap between the generated and reference summaries. It does so by measuring the recall of unigrams (ROUGE-1), bigrams (ROUGE-2), and the longest common subsequence (ROUGE-L). This helps determine how much key information from the reference summary is present in the generated summary. Higher ROUGE scores mean greater similarity between summaries, indicating higher quality.'
 
@@ -18,15 +19,15 @@ st.title("Hugging Face Text Summarization")
 # Description
 st.write("This application summarizes the input text using DistilBERT.")
 
-inputs = tokenizer(input_text, return_tensors="pt")
+# inputs = tokenizer(input_text, return_tensors="pt")
 
-# Generate text using the model
-output = model.generate(inputs['input_ids'], max_length=100)
+# # Generate text using the model
+# output = model.generate(inputs['input_ids'], max_length=100)
 
-# Decode the generated text
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+# # Decode the generated text
+# generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
-st.write(generated_text)
+# st.write(generated_text)
 
 # Text input from the user
 # user_input = st.text_area("Enter the text you want to summarize", height=250)
