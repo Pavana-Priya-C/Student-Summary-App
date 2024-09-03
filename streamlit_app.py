d217e7f2c09b9b03802926f55815837eb7e70a76
Pavana <pavanapc.riverdale@gmail.com>
2024-09-03 22:32:44 +0530

    add files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,332 +1,8 @@
-# import streamlit as st
-# from streamlit_option_menu import option_menu
-# import os
-# from PIL import Image
-
-
-# # Set the title of the web app
-# st.title("Welcome to Future Minds Tutoring")
-
-# # Sidebar with navigation options
-# with st.sidebar:
-#     option = option_menu("Menu",
-#                         options=["About Us","Chapter Summary"],
-#                         icons=['house-fill','search'])
-
-# if option == "About Us":
-#     # Load and resize the image
-#     img = Image.open("images/Future min.jpg")
-    
-#     # Specify the desired height
-#     desired_height = 250
-#     # Calculate the width to maintain the aspect ratio
-#     width, height = img.size
-#     aspect_ratio = width / height
-#     new_width = int(desired_height * aspect_ratio)
-    
-#     # Resize the image
-#     img = img.resize((new_width, desired_height))
-    
-#     # Display the resized image
-#     st.image(img, use_column_width=True)
-
-#     st.header("About Us")
-#     st.write("""
-#     At Future Minds Tutoring, we deliver outstanding educational support to students in grades 10, 11, and 12. Our innovative methodology blends conventional pedagogy with state-of-the-art artificial intelligence technologies to provide all-encompassing and customized learning experiences.
-
-#     Our professional educators and AI specialists collaborate to generate chapter-specific content that is simplified and easy to understand, allowing students to grasp and retain information more quickly. Our dedication lies in providing specialized resources and support to kids with exceptional needs, enabling them to overcome obstacles and excel academically.
-#     """)
-
-#     st.header("Mission")
-#     st.write("""
-#     Our mission is to provide high-quality, inclusive education to students of classes 10, 11, and 12, fostering an environment where every student, including those with special needs, can excel. We leverage advanced AI technology to deliver personalized and effective learning experiences that help students understand and retain complex concepts.
-#     """)
-    
-#     st.header("Vision")
-#     st.write("""
-#     Our vision is to be a leading educational institution recognized for our commitment to academic excellence and inclusivity. We aim to transform traditional learning by integrating innovative AI solutions, ensuring that every student, regardless of their challenges, has the opportunity to succeed and achieve their full potential.
-#     """)
-    
-    
-
-# elif option == "Chapter Summary":
-#     st.subheader("Chapter Summary")
-#     chapter_num = st.selectbox("Choose the chapter for reading",
-#                                            options=['Chapter 1 - A Triumph of Surgery',
-#                                                     'Chapter 2 - The Theif\'s Story',
-#                                                     'Chapter 3 - The midnight visitor',
-#                                                     'Chapter 4 - A Question of Trust',
-#                                                     'Chapter 5 - Footprints with Feet',
-#                                                     'Chapter 6 - The Making of a Scientist',
-#                                                     'Chapter 7 - The Necklace',
-#                                                     'Chapter 8 - Bholi',
-#                                                     'Chpater 9 - The Book That Saved the Earth'],
-#                                                     index=None)
-    
-#     if chapter_num == 'Chapter 1 - A Triumph of Surgery':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap1-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap1-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2) 
-        
-#         st.write("")
-#         st.write("")
-#         st.write("")
-
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap1_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)     
-
-#     elif chapter_num == 'Chapter 2 - The Theif\'s Story':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap2-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap2-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)  
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')        
-#         img = Image.open("images/chap2_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)       
-
-#     elif chapter_num == 'Chapter 3 - The midnight visitor':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap3-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap3-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)  
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')        
-#         img = Image.open("images/chap3_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)       
-
-#     elif chapter_num == 'Chapter 4 - A Question of Trust':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap4-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap4-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)  
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap4_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)     
-
-#     elif chapter_num == 'Chapter 5 - Footprints with Feet':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap5-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap5-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)  
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap5_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)             
-
-#     elif chapter_num == 'Chapter 6 - The Making of a Scientist':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap6-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap6-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2) 
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap6_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)       
-
-#     elif chapter_num == 'Chapter 7 - The Necklace':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap7-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap7-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)  
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap7_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)                                                  
-
-#     elif chapter_num == 'Chapter 8 - Bholi':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap8-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap8-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)  
-        
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap8_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)     
-
-#     elif chapter_num == 'Chpater 9 - The Book That Saved the Earth':
-#         tab1,tab2 = st.tabs(['Quick Look 1', 'Quick Look 2'])
-#         with tab1:
-#             file_path = "data/chap9-t5base.txt"
-#             with open(file_path, "r") as file:
-#                 file_content1 = file.read()
-
-#             # Display the file content
-#             st.write(file_content1)
-
-#         with tab2:
-#             file_path = "data/chap9-gemini.txt"
-#             with open(file_path, "r") as file:
-#                 file_content2 = file.read()
-
-#             # Display the file content
-#             st.write(file_content2)    
-
-#         st.write("")
-#         st.write("")
-#         st.write("")
-        
-#         st.subheader('You’ve read the story, now let’s explore the key words.')
-        
-#         img = Image.open("images/chap9_kw.jpg")
-#         # Display the resized image
-#         st.image(img, use_column_width=True)         
-
-
-
-
-
-
-# # Footer
-# st.write("")
-# st.write("")
-# st.write("")
-# st.write("")
-# st.markdown("***")
-# st.write("© 2024 Future Minds Tutoring. All rights reserved.")  
-
 import streamlit as st
+from streamlit_option_menu import option_menu
+import os
+from PIL import Image
+from preprocess import extract_text_from_pdf, preprocess_text_chap8, preprocess_text, get_title, remove_subsequent_occurrences, separate_sections
 from transformers import BartForConditionalGeneration, BartTokenizer
 
 # Load the BART model and tokenizer
@@ -345,31 +21,114 @@ def chunk_text(text, chunk_size=512):
     for i in range(0, len(words), chunk_size):
         yield " ".join(words[i:i + chunk_size])
 
-# Streamlit UI
-st.title("Text Summarization using BART")
 
-# Upload text file
-uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
+# Set the title of the web app
+st.title("Welcome to Future Minds Tutoring")
 
-if uploaded_file is not None:
-    # Read the uploaded text file
-    text = uploaded_file.read().decode("utf-8")
+# Sidebar with navigation options
+with st.sidebar:
+    option = option_menu("Menu",
+                        options=["About Us","Chapter Summary"],
+                        icons=['house-fill','search'])
+
+if option == "About Us":
+    # Load and resize the image
+    img = Image.open("images/Future min.jpg")
     
-    # Display the original text
-    st.subheader("Original Text")
-    st.write(text)
+    # Specify the desired height
+    desired_height = 250
+    # Calculate the width to maintain the aspect ratio
+    width, height = img.size
+    aspect_ratio = width / height
+    new_width = int(desired_height * aspect_ratio)
+    
+    # Resize the image
+    img = img.resize((new_width, desired_height))
+    
+    # Display the resized image
+    st.image(img, use_column_width=True)
 
-    # Split the text into smaller chunks and summarize each chunk
-    summarized_text = ""
-    for chunk in chunk_text(text):
-        inputs = tokenizer(chunk, max_length=1024, return_tensors="pt", truncation=True)
-        summary_ids = model.generate(inputs["input_ids"], num_beams=4, min_length=30, max_length=200, length_penalty=2.0)
-        summarized_chunk = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-        summarized_text += summarized_chunk + " "
+    st.header("About Us")
+    st.write("""
+    At Future Minds Tutoring, we deliver outstanding educational support to students in grades 10, 11, and 12. Our innovative methodology blends conventional pedagogy with state-of-the-art artificial intelligence technologies to provide all-encompassing and customized learning experiences.
 
-    # Display the summarized text
-    st.subheader("Summarized Text")
-    st.write(summarized_text)
+    Our professional educators and AI specialists collaborate to generate chapter-specific content that is simplified and easy to understand, allowing students to grasp and retain information more quickly. Our dedication lies in providing specialized resources and support to kids with exceptional needs, enabling them to overcome obstacles and excel academically.
+    """)
+
+    st.header("Mission")
+    st.write("""
+    Our mission is to provide high-quality, inclusive education to students of classes 10, 11, and 12, fostering an environment where every student, including those with special needs, can excel. We leverage advanced AI technology to deliver personalized and effective learning experiences that help students understand and retain complex concepts.
+    """)
+    
+    st.header("Vision")
+    st.write("""
+    Our vision is to be a leading educational institution recognized for our commitment to academic excellence and inclusivity. We aim to transform traditional learning by integrating innovative AI solutions, ensuring that every student, regardless of their challenges, has the opportunity to succeed and achieve their full potential.
+    """)
+    
+    
+
+elif option == "Chapter Summary":
+    st.subheader("Chapter Summary")
+    # File upload section
+        
+    uploaded_file = st.file_uploader("Upload your file :", type=["pdf"])
+
+    if uploaded_file is not None:
+        filename = uploaded_file.name
+
+        if uploaded_file.type == "application/pdf":
+
+            # st.write(f"File Name: {filename}")
+            st.success("File uploaded successfully! Please wait for the summary...")
+            if filename == 'jefp108.pdf':
+                # Extract text from the uploaded PDF file
+                raw_text = extract_text_from_pdf(uploaded_file)
+
+                preprocessed_text = preprocess_text_chap8(raw_text)
+                preprocessed_text = preprocess_text(preprocessed_text)
+                title='Bholi'
+                
+                 # Remove subsequent occurrences of the title
+                clean_text = remove_subsequent_occurrences(preprocessed_text, title)
+
+                # Separate sections of the text
+                main_content, glossary, think_about_it, talk_about_it, suggested_reading = separate_sections(clean_text)
+                    
+                # Split the text into smaller chunks and summarize each chunk
+                summarized_text = ""
+                for chunk in chunk_text(main_content):
+                    inputs = tokenizer(chunk, max_length=1024, return_tensors="pt", truncation=True)
+                    summary_ids = model.generate(inputs["input_ids"], num_beams=4, min_length=30, max_length=200, length_penalty=2.0)
+                    summarized_chunk = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+                    summarized_text += summarized_chunk + " "
+
+                st.subheader(f'Summary of {title}: \n {summarized_text}')   
+
+                         
+            else:
+                st.write("Please upload other file....")        
+
+        else:
+            st.error("Please upload a valid PDF file.")
+            
+
+    else:
+        st.warning("Please upload a file.")     
+
+
+
+
+
+
+# Footer
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.markdown("***")
+st.write("© 2024 Future Minds Tutoring. All rights reserved.")  
+
+
 
 
 
